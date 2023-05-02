32059efe95cc4251473707f88e1182fbb4956c22
designAR <dkfkddl95@naver.com>
2023-05-03 01:47:49 +0900

    Initial commit

---
+++ src/main/java/senior/seniorjobspring/SeniorjobSpringApplication.java
@@ -0,0 +1,13 @@
+package senior.seniorjobspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorjobSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorjobSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senior/seniorjobspring/SeniorjobSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package senior.seniorjobspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorjobSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
